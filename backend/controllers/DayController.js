4fe3a1d1a19c8ba6cf71146c3d168bbe047e5c82
Omer <omerg863@gmail.com>
2022-07-18 22:21:12 +0300

    :sparkles: added functions to backend

--- backend/controllers/DayController.js
+++ backend/controllers/DayController.js
@@ -11,7 +11,7 @@ const getDay = asyncHandler(async (req, res, next) => {
     });
 });
 
-const checkAppointments = async (appointments, time) => {
+const checkAppointments = (appointments, time) => {
     appointments.forEach(appoint => {
         if (appoint.time === time) {
             return appoint;
@@ -20,16 +20,81 @@ const checkAppointments = async (appointments, time) => {
     return false;
 }
 
-const getDayAppointments = asyncHandler(async (req, res, next) => {
-    var day = await Day.findOne({ date: req.params.date }).populate('appointments', ['user', 'time', '_id']);
-    if (!day){
-        day = await Day.create({ date: req.params.date, start_time: "08:00", end_time: "20:00", interval: "30m", breaks: ["10:00", "14:00"], appointments: [] });
+const checkBreaks = (breaks, time) => {
+    let returnValue = false;
+    for (let i = 0; i < breaks.length; i++) {
+        if (breaks[i] === time) {
+            returnValue = true;
+            break;
+        }
     }
-    var Schedule = [];
+    return returnValue;
+}
+
+const checkZeroNeeded = (num) => {
+    if (parseInt(num) < 10) {
+        return `0${num}`;
+    }
+    return num;
+}
+
+const createAppointmentsList = (date, start_time, end_time, interval) => {
+    let intervalMinutes;
+    if (interval.includes('m')) {
+        intervalMinutes = parseInt(interval.replace('m', ''));
+    } else {
+        intervalMinutes = parseInt(interval.replace('h', '')) * 60;
+    }
+    let appointments = [];
+    let start_time_date = new Date(date.getDate(), date.getMonth(), date.getFullYear(), start_time.split(':')[0], start_time.split(':')[1]);
+    let end_time_date = new Date(date.getDate(), date.getMonth(), date.getFullYear(), end_time.split(':')[0], end_time.split(':')[1]);
+    while (start_time_date <= end_time_date) {
+        appointments.push(checkZeroNeeded(start_time_date.getHours()) + ':' + checkZeroNeeded(start_time_date.getMinutes()));
+        start_time_date.setMinutes(start_time_date.getMinutes() + intervalMinutes);
+    }
+    return appointments;
+}
+
+const calculateDay = (free_appointments, breaks, appointments) => {
+    let schedule = [];
+    free_appointments.forEach(appoint => {
+        let checkAppointment = checkAppointments(appointments, appoint);
+        if (checkAppointment) {
+            schedule.push({
+                user: checkAppointment.user,
+                time: appoint,
+            });
+        }
+        let checkBreak = checkBreaks(breaks, appoint);
+        if (checkBreak) {
+            schedule.push({
+                time: appoint,
+                type: 'break'
+            });
+        }
+        if (!checkAppointment && !checkBreak) {
+            schedule.push({
+                time: appoint,
+                type: 'free'
+            });
+        }
+    });
+    return schedule;
+}
+
+const getDayAppointments = asyncHandler(async (req, res, next) => {
+    const dateFormatted = req.params.date.split('-');
+    const date = new Date(parseInt(dateFormatted[0]), parseInt(dateFormatted[1]) - 1, parseInt(dateFormatted[2]));
+    var day = await Day.findOne({ date: date }).populate('appointments', ['user', 'time', '_id']);
+    if (!day){
+        day = await Day.create({ date: date, startTime: "08:00", endTime: "20:00", interval: "30m", breaks: ["10:00", "14:00"], appointments: [] });
+    }
+    let free_appointments = createAppointmentsList(day.date, day.startTime, day.endTime, day.interval);
+    let schedule = calculateDay(free_appointments, day.breaks, day.appointments);
     res.status(200).json({
         success: true,
-        day
+        schedule
     });
 });
 
-export { getDay };
+export { getDay, getDayAppointments };
--- backend/routes/DayRoutes.js
+++ backend/routes/DayRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { protectUser } from '../middleware/AuthMiddleware.js';
-import { getDay } from '../controllers/DayController.js';
+import { protectUser, protectStaff } from '../middleware/AuthMiddleware.js';
+import { getDay, getDayAppointments } from '../controllers/DayController.js';
 
 const router = express.Router();
 
 
 router.get('/get/:date', protectUser, getDay);
+router.get('/getDay/:date', protectStaff, getDayAppointments);
+
 
 export default router;
